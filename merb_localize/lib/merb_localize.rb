c5b2feb6bb2c42eea34662849e07973527cfcf68
Martin Grund <grund@MacGrund.local>
2008-03-25 10:13:42 +0100

    * initial commit for localize

---
+++ merb_localize/lib/merb_localize.rb
@@ -0,0 +1,36 @@
+if defined?(Merb::Plugins)  
+  
+  # Ruby unicode support
+  $KCODE = 'u'
+  require 'jcode'
+  
+  dependency "gettext", '>= 1.9'
+  require 'gettext/utils'
+  
+  # global methods
+  # all in the GettextLocalize module
+  require 'gettext_localize/gettext_localize'
+
+  # locale used in the literals to be translated
+  GettextLocalize::original_locale = 'en'
+  # locale in case everything else fails
+  GettextLocalize::fallback_locale = 'ca'
+  # country if everything else fails
+  GettextLocalize::fallback_country = 'es'
+
+  # add this plugin as a new textdomain
+  # add this line to every localized plugin
+  #GettextLocalize::plugin_bindtextdomain
+
+  # initialize country options from YML file
+  GettextLocalize::set_country_options
+
+  # base ruby class extensions
+  require 'gettext_localize/gettext_localize_extend'
+
+  # set paths with LC_MESSAGES
+  GettextLocalize::set_locale_paths
+
+  
+  Merb::Plugins.add_rakefiles(File.join(File.dirname(__FILE__) / "tasks" / "gettext_tasks" ))
+end
